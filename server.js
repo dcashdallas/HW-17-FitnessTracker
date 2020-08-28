ec4b52b41790044ea59b7245001010dfcb220b8e
dcashdallas <dcashdallas81@yahoo.com>
2020-08-28 02:00:13 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/quiet-ocean-05267", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hw-17-fitnesstracker", { useNewUrlParser: true });
 
 const db = require("./models");
 
