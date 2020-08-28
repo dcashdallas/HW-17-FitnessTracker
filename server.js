f123089f9ffc01e53a027b71d1dd00e7aa7cd109
dcashdallas <dcashdallas81@yahoo.com>
2020-08-28 01:40:38 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/quiet-ocean-05267", { useNewUrlParser: true });
 
 const db = require("./models");
 
